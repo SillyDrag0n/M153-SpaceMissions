ae4ad28b3bb904ad422e185dd42ba766ffbb961d
Silvan Müller <silvan.mueller@bossinfo.ch>
2023-04-18 16:21:03 +0200

    move created StrdFnctns into the correct file

---
+++ 03_SP_SF_Scripts/CreateStoredFunctionPersonalWorkedOnMission_.sql
@@ -0,0 +1,29 @@
+--*********************************************************
+-- SpacePrograms
+--*********************************************************
+
+------------------------------------------------------------------
+-- Stored Function 'fn_PersonalWorkedOnMission' erstellen
+-- Searches for all Personal that worked on a selected mission
+------------------------------------------------------------------
+
+-- drop function if exists fn_PersonalWorkedOnMission;
+-- go
+ 
+create function fn_PersonalWorkedOnMission (@MissionId int) returns table as 
+	return 
+		select 
+			Mission.MissionId, 
+			Mission.MissionName, 
+			(Personal.PersonalFirstname + ' ' + PersonalLastname ) AS PersonalName, 
+			Job.JobDescription
+			from 
+				Personal 
+				inner join Worked on Personal.PersonalId = Worked.fk_PersonalId
+				inner join Mission on Worked.fk_MissionId = Mission.MissionId
+				inner join Job on Personal.fk_JobId = Job.JobId
+			where 
+				Mission.MissionId = @MissionId;
+
+-- SELECT * FROM [dbo].[fn_PersonalWorkedOnMission] (1)
+-- GO
---
+++ 03_SP_SF_Scripts/CreateStoredFunctionUncrewedMissions.sql
@@ -0,0 +1,26 @@
+--*********************************************************
+-- SpacePrograms
+--*********************************************************
+
+------------------------------------------------------------------
+-- Stored Function 'fn_UncrewedMissions' erstellen
+-- Searches for all missions without any crew members registered
+------------------------------------------------------------------
+
+drop function if exists fn_UncrewedMissions;
+go
+
+create function fn_UncrewedMissions () returns table as 
+    return
+        select 
+            * 
+        from 
+            Mission 
+        where not exists
+            (select * 
+                from Worked
+                where Mission.MissionId = Worked.fk_MissionId
+            );
+
+-- SELECT * FROM [dbo].[fn_UncrewedMissions] ()
+-- GO
