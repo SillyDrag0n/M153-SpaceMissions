1818580d867f1e4aab741d972a802833c5f6abec
Silvan Müller <silvan.mueller@bossinfo.ch>
2023-04-18 10:49:51 +0200

    add example for a Stored Procedure

--- 03_SP_SF_Scripts/SpaceProgramCreateStoredProcedure.sql
+++ 03_SP_SF_Scripts/SpaceProgramCreateStoredProcedure.sql
@@ -5,3 +5,18 @@
 -----------------------------------------------------------
 -- Stored Procedures erstellen
 -----------------------------------------------------------
+
+-- use ComputerShop
+-- go
+-- 
+-- drop function if exists fn_KundenName;
+-- go
+-- 
+-- create function fn_KundenName (@KundeId int) returns varchar(100) as 
+-- begin
+-- declare @KundenName varchar (100);
+-- 	set @KundenName = (select (Kunde.Nachname + ' ' + Kunde.Vorname) AS Kunde 
+-- 					   from Kunde 
+-- 					   where Kunde.KundeId = @KundeId);
+-- 	return @KundenName;
+-- end;
