fc2c46935de24fdc669c5613fa60dd6c61665173
Silvan Müller <silvan.mueller@bossinfo.ch>
2023-04-19 10:53:00 +0200

    add function fn_IsNotNullOrEmpty

---
+++ M153_SpaceProgram_fn_IsNotNullOrEmpty.sql
@@ -0,0 +1,37 @@
+--*********************************************************
+-- SpaceProgram
+--*********************************************************
+
+------------------------------------------------------------------
+-- Stored Function 'fn_IsNotNullOrEmpty' erstellen
+-- Checks if a given Parameter is either NULL or empty 
+-- returns either a 1 (true) or 0 (false)
+------------------------------------------------------------------
+
+-- drop function if exists fn_IsNotNullOrEmpty;
+-- go
+
+create function fn_IsNotNullOrEmpty (@varchar varchar(60)) returns bit as
+begin
+	declare @bool bit = null;
+	if @varchar is not null and LEN(@varchar) > 0
+	    set @bool = 1;
+	else
+		set @bool = 0;
+	return @bool;
+end
+go
+
+------------------------------------------------------------------
+-- 
+-- Tests for stored Function 'fn_IsNotNullOrEmpty'
+--
+------------------------------------------------------------------
+
+-- Should return 0 (false)
+-- SELECT [dbo].[fn_IsNotNullOrEmpty] ('')
+-- returns 0
+
+-- Should return 1 (true)
+-- SELECT [dbo].[fn_IsNotNullOrEmpty] ('test')
+-- returns 1
