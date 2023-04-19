b6ac7aa9567da746da12fe73e889fdd4d3f38493
Silvan Müller <silvan.mueller@bossinfo.ch>
2023-04-19 14:29:47 +0200

    rename fn_IsNotNullOrEmpty into fn_IsNullOrEmpty

--- M153_SpaceProgram_fn_IsNullOrEmpty.sql
+++ M153_SpaceProgram_fn_IsNullOrEmpty.sql
@@ -1,20 +1,22 @@
 --*********************************************************
--- SpaceProgram
+-- SpacePrograms
 --*********************************************************
 
 ------------------------------------------------------------------
--- Stored Function 'fn_IsNotNullOrEmpty' erstellen
+-- Stored Function 'fn_IsNullOrEmpty' erstellen
 -- Checks if a given Parameter is either NULL or empty 
 -- returns either a 1 (true) or 0 (false)
 ------------------------------------------------------------------
-
-drop function if exists fn_IsNotNullOrEmpty;
+use SpacePrograms
 go
 
-create function fn_IsNotNullOrEmpty (@varchar varchar(60)) returns bit as
+drop function if exists fn_IsNullOrEmpty;
+go
+
+create function fn_IsNullOrEmpty (@varchar varchar(60)) returns bit as
 begin
 	declare @bool bit = null;
-	if @varchar is not null and LEN(@varchar) > 0
+	if @varchar is null and LEN(@varchar) > 0
 	    set @bool = 1;
 	else
 		set @bool = 0;
@@ -28,12 +30,12 @@ go
 --
 ------------------------------------------------------------------
 
--- Should return 0 (false)
-select [dbo].[fn_IsNotNullOrEmpty] ('')
-go
--- returns 0
-
 -- Should return 1 (true)
-select [dbo].[fn_IsNotNullOrEmpty] ('test')
+select [dbo].[fn_IsNullOrEmpty] ('')
 go
 -- returns 1
+
+-- Should return 0 (false)
+select [dbo].[fn_IsNullOrEmpty] ('test')
+go
+-- returns 0
