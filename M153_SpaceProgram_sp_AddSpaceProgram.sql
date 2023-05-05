e943ccfc72d3b3c6df42d18ddeea477ff8494a81
Silvan Müller <silvan.mueller@bossinfo.ch>
2023-05-05 11:44:44 +0200

    add sp_AddSpaceProgramTests and validation

--- M153_SpaceProgram_sp_AddSpaceProgram.sql
+++ M153_SpaceProgram_sp_AddSpaceProgram.sql
@@ -37,10 +37,18 @@ as begin
         return
     end
 
-    if (@ProgramEndDate IS NOT NULL and @ProgramEndDate < @ProgramStartDate and ISDATE(Cast(@ProgramStartDate as varchar)) = 0)
+    if (@ProgramEndDate IS NOT NULL)
     begin
-        raiserror('Invalid parameter: @ProgramEndDate Must be a higher date than @ProgramStartDate. Must be yyyy-mm-dd', 16, 0)
-        return
+        if(ISDATE(Cast(@ProgramEndDate as varchar)) = 0)
+        begin 
+            raiserror('Invalid parameter: If @ProgramEndDate is declared, it must have the following format: yyyy-mm-dd', 16, 0)
+            return
+        end
+        if(@ProgramStartDate > @ProgramEndDate)
+        begin 
+            raiserror('Invalid parameter: @ProgramEndDate Must be greater than @ProgramStartDate. Must be yyyy-mm-dd', 16, 0)
+            return
+        end
     end
 
     if (ISNULL(@ProgramBudget, 0) = 0 or SQL_VARIANT_PROPERTY(@ProgramBudget, 'Precision') > 16 or SQL_VARIANT_PROPERTY(@ProgramBudget, 'Scale') > 2)
@@ -75,12 +83,36 @@ go
 -- Tests for stored procedure 'sp_AddSpaceProgram'
 --
 ------------------------------------------------------------------
--- declare @ProgramName_l varchar(50) = 'TestProgram'
--- declare @ProgramStartDate_l date = '2004-04-26'
--- declare @ProgramBudget_l decimal(16,2) = 10500.00
--- declare @ProgramNoOfFlights_l int = 3
--- 
--- exec sp_AddSpaceProgram @ProgramName_l, @ProgramStartDate_l, @ProgramBudget_l, @ProgramNoOfFlights_l;
 
-execute dbo.sp_AddSpaceProgram 'Test Flight Program', '2023-03-05', NULL, 500.00, 3
+-- Inserts a new entry into the Program table 
+
+declare @ProgramName_l varchar(50) =       'Test Flight Program'
+declare @ProgramStartDate_l date =         '2004-04-26'
+declare @ProgramEndDate_l date =           '2004-04-26'
+declare @ProgramBudget_l decimal(16,2) =   1050000.00
+declare @ProgramNoOfFlights_l int =        9
+
+exec sp_AddSpaceProgram @ProgramName_l, @ProgramStartDate_l, @ProgramEndDate_l, @ProgramBudget_l, @ProgramNoOfFlights_l;
+go
+
+-- Inserts a new entry into the Program table 
+
+declare @ProgramName_l varchar(50) =       'Moon Crater Program'
+declare @ProgramStartDate_l date =         '2017-08-02'
+declare @ProgramEndDate_l date =            NULL
+declare @ProgramBudget_l decimal(16,2) =    650000.00
+declare @ProgramNoOfFlights_l int =         2
+
+exec sp_AddSpaceProgram @ProgramName_l, @ProgramStartDate_l, @ProgramEndDate_l, @ProgramBudget_l, @ProgramNoOfFlights_l;
+go
+
+-- Returns an error because @ProgramStartDate_l can not be greater than @ProgramEndDate_l
+
+declare @ProgramName_l varchar(50) =       'The Faulty Program'
+declare @ProgramStartDate_l date =         '2023-05-05'
+declare @ProgramEndDate_l date =            '1999-02-12'
+declare @ProgramBudget_l decimal(16,2) =    1291.00
+declare @ProgramNoOfFlights_l int =         5
+
+exec sp_AddSpaceProgram @ProgramName_l, @ProgramStartDate_l, @ProgramEndDate_l, @ProgramBudget_l, @ProgramNoOfFlights_l;
 go
