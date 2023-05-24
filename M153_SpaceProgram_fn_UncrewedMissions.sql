2efca7f6f7481726037786ab53e3c28c26b69463
SilMueller <silvan.mueller@bossinfo.ch>
2023-05-24 08:50:24 +0200

    refactor tests for fn_UncrewedMissions

--- M153_SpaceProgram_fn_UncrewedMissions.sql
+++ M153_SpaceProgram_fn_UncrewedMissions.sql
@@ -1,5 +1,6 @@
 --*********************************************************
 -- SpacePrograms
+-- TODO SIMU: Add third test for this function
 --*********************************************************
 
 ------------------------------------------------------------------
@@ -24,12 +25,15 @@ create function fn_UncrewedMissions () returns table as
                 where Mission.MissionId = Worked.fk_MissionId
             );
 go
-------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
 -- 
 -- Tests for stored Function 'fn_UncrewedMissions'
 --
-------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------------------------
+-- Test 1: Returns all missions without any personal assigned to it
+--
 -- Should return a set of data similar to this:
 -- MissionId     MissionName     MissionSucceeded   fk_ProgramId fk_DestinationId fk_LaunchSite
 -- 10	         Kosmos 57	     0	                6	          19	          3
@@ -37,16 +41,32 @@ go
 -- 14	         Luna Nights II	 0	                9	          13	          4
 -- 15	         Luna Nights III 1	                9	          2	              4
 -- 17	         Mir 1	         1	                11 	          16	          3
+--------------------------------------------------------------------------------------------------
 
 select * from [dbo].[fn_UncrewedMissions] ()
 go
 
--- returns:
+
+--------------------------------------------------------------------------------------------------
+-- Manipulate data for second test
+--------------------------------------------------------------------------------------------------
+insert into Worked
+	(fk_MissionId,                                                                                  fk_PersonalId) values
+	((select MissionId from Mission where MissionName = 'Luna Nights II'),                          (select PersonalId from Personal where PersonalFirstname = 'Edward' and PersonalLastname = 'Higgins White II')),
+	((select MissionId from Mission where MissionName = 'Luna Nights II'),                          (select PersonalId from Personal where PersonalFirstname = 'Martinez' and PersonalLastname = 'De Loca')),
+	((select MissionId from Mission where MissionName = 'Luna Nights II'),                          (select PersonalId from Personal where PersonalFirstname = 'Walter' and PersonalLastname = 'White')),
+	((select MissionId from Mission where MissionName = 'Luna Nights III'),                         (select PersonalId from Personal where PersonalFirstname = 'Amporio' and PersonalLastname = 'Alnino')),
+	((select MissionId from Mission where MissionName = 'Luna Nights III'),                         (select PersonalId from Personal where PersonalFirstname = 'Edward' and PersonalLastname = 'Higgins White II'));
+	
+--------------------------------------------------------------------------------------------------
+-- Test 2: Missions 'Luna Nights II' and 'Luna Nights III' shouldn't appear now
+--
+-- Should return a set of data similar to this:
 -- MissionId     MissionName     MissionSucceeded   fk_ProgramId fk_DestinationId fk_LaunchSite
 -- 10	         Kosmos 57	     0	                6	          19	          3
 -- 11	         Voshkod 1	     1	                6	          14	          3
--- 14	         Luna Nights II	 0	                9	          13	          4
--- 15	         Luna Nights III 1	                9	          2	              4
 -- 17	         Mir 1	         1	                11 	          16	          3
+--------------------------------------------------------------------------------------------------
 
-------------------------------------------------------------------
+select * from [dbo].[fn_UncrewedMissions] ()
+go
